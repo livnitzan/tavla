71a5e8bc91b66d0ca36a9321ac06884be0687485
livnitzan <livnitzan@gmail.com>
2026-01-22 16:06:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from google.cloud import bigquery
 from google.oauth2 import service_account
+from modules.logic import get_season_data, get_filter_options
 import os
 
 # 1. הגדרות עמוד (RTL ופריסה רחבה)
